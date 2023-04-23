c51f1c319e2fe3a889b301fb45fff7e3b237a68d
Zmaplex <zmaplex@gmail.com>
2023-04-23 14:33:14 +0800

    Updated:  breaking

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 init:
 	apt install  s3fs python3-venv -y
 	python3 -m venv venv
-	rm -rf /etc/systemd/system/ftp_server.service
+	rm -rf /etc/systemd/system/ftp_server.service /etc/systemd/system/ftp-server.service
 	venv/bin/pip install -U pip wheel setuptools
 	venv/bin/pip install -r requirements.txt
 	cp example.config.json config.json 
 	openssl req -new -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out cert.pem  -keyout key.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=uissh.com"
 
 install:
-	cp ftp_server.service /lib/systemd/system/ftp_server.service
-	ln -s /lib/systemd/system/ftp_server.service  /etc/systemd/system/ftp_server.service
+	cp ftp-server.service /lib/systemd/system/ftp-server.service
+	ln -s /lib/systemd/system/ftp-server.service  /etc/systemd/system/ftp-server.service
 	systemctl daemon-reload
-	systemctl enable --now ftp_server
+	systemctl enable --now ftp-server
--- src/ftp_server/s3.py
+++ src/ftp_server/s3.py
@@ -8,7 +8,20 @@ from src.ftp_server.helper import add_user
 DEBUG = os.environ.get('debug', False)
 
 def load_s3(authorizer: DummyAuthorizer, data: dict):
-
+    """
+    :param authorizer
+    :param data: {
+        "user": "s3",
+        "pass": "s3",
+        "fs": "s3",
+        "params": {
+            "basePath": "/s3-fuse",
+            "bucket": "my-bucket",
+            "access_key_id": "AKIA....",
+            "secret_access_key": "IDxd...."
+        }
+    }
+    """
     user = data.get('user')
     access_key_id = data.get('params').get('access_key_id')
     secret_access_key = data.get('params').get('secret_access_key')
@@ -19,12 +32,15 @@ def load_s3(authorizer: DummyAuthorizer, data: dict):
     os.system(f'chmod 600 {passwd}')
     bucket = data.get('params').get('bucket')
 
-    # 兼容旧版本配置
+    # if basePath is not set, use bucket name as basePath
     base_path = data.get('params').get('basePath',f'/{bucket}')
+    
+    
     os.system(f'mkdir -p {base_path}')
     cmd = f's3fs {bucket} {base_path} -o passwd_file={passwd} -o use_cache=/tmp'
+    
     if DEBUG:
         cmd += ' -o dbglevel=info -f -o curldbg'
-
+        
     Thread(target=os.system, args=(cmd,)).start()
     add_user(authorizer, data, base_path)
